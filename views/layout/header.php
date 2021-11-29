ef4edcb2d60391dc135faa4eb4effc604eca5dfb
mauricio <92113715+maurimontoya@users.noreply.github.com>
2021-11-29 12:31:28 -0300

    Initial commit

---
+++ views/layout/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML>
+<html lang="es">
+	<head>
+		<meta charset="utf-8" />
+		<title>ICELL LP</title>
+		<link rel="stylesheet" href="<?=base_url?>assets/css/styles.css" />
+	</head>
+	<body>
+		<div id="container">
+			<!-- CABECERA -->
+			<header id="header">
+				<div id="logo">
+					<img src="<?=base_url?>assets/img/icell.jpg" alt="Icell Logo" />
+					<a href="<?=base_url?>">
+						ICELL LP
+					</a>
+				</div>
+			</header>
+
+			<!-- MENU -->
+			<?php $categorias = Utils::showCategorias(); ?>
+			<nav id="menu">
+				<ul>
+					<li>
+						<a href="<?=base_url?>">Inicio</a>
+					</li>
+					<?php while($cat = $categorias->fetch_object()): ?>
+						<li>
+							<a href="<?=base_url?>categoria/ver&id=<?=$cat->id?>"><?=$cat->nombre?></a>
+						</li>
+					<?php endwhile; ?>
+				</ul>
+			</nav>
+
+			<div id="content">
